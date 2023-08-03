a22c186e5863ea5fa4e6d6a488ea2b48b34bf2aa
Svulster <140300305+Svulster@user.noreply.github.com>
2023-08-03 22:52:16 +0200

    Backup

--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 import pygame
 from pygame.sprite import Group
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
+
+        self.import_assets()
 
         #general setup
         self.image = pygame.Surface((32, 64))
@@ -15,6 +18,18 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [],'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [],'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [],'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [],'up_water': [], 'down_water': [],}
+        
+        for animation in self.animations.keys():
+            full_path = '/assets/graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
+        print(self.animations)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -34,8 +49,18 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
 
     def move(self, dt):
-        self.pos += self.direction * self.speed * dt
-        self.rect.center = self.pos
+
+        #normalizing a vector
+        if self.direction.magnitude()>0:
+            self.direction = self.direction.normalize()
+
+        #horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        #vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
 
     def update(self, dt):
         self.input()
