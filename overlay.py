3ac1c12f40297a7521e75de1efe6a5bbc57e3923
Svulster <140300305+Svulster@user.noreply.github.com>
2023-08-07 23:13:25 +0200

    Added level sprites

--- overlay.py
+++ overlay.py
@@ -9,7 +9,7 @@ class Overlay:
         self.player = player
 
         # imports
-        overlay_path = "./assets/overlay/"
+        overlay_path = "./assets/graphics/overlay/"
         self.tools_surf = {tool: pygame.image.load(f'{overlay_path}{tool}.png').convert_alpha() for tool in player.tools}
         self.seeds_surf = {seed: pygame.image.load(f'{overlay_path}{seed}.png').convert_alpha() for seed in player.seeds}
 
